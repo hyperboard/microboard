96124303fddf8ad1f8b1b8533a0754b51ee575d8
Alexandr Panchenko <alexandr.panchenko.93@yandex.ru>
2025-06-11 12:48:42 +0300

    fix imports

--- src/Events/MessageRouter/handleBoardEventMessage.ts
+++ src/Events/MessageRouter/handleBoardEventMessage.ts
@@ -1,41 +1,43 @@
-import { Board } from 'Board';
-import { conf } from 'Settings';
-import { SyncEvent } from '../Events';
-import { Storage } from 'App/Storage';
+import { Board } from "Board";
+import { conf } from "Settings";
+import { SyncEvent } from "../Events";
 
 export interface BoardEventMsg {
-	type: 'BoardEvent';
-	boardId: string;
-	event: SyncEvent;
-	sequenceNumber: number;
-	userId: string;
+  type: "BoardEvent";
+  boardId: string;
+  event: SyncEvent;
+  sequenceNumber: number;
+  userId: string;
 }
 
-export function handleBoardEventMessage(message: BoardEventMsg, board: Board): void {
-	if (!board.events) {
-		return;
-	}
-	const { log } = board.events;
-	const event = message.event;
+export function handleBoardEventMessage(
+  message: BoardEventMsg,
+  board: Board
+): void {
+  if (!board.events) {
+    return;
+  }
+  const { log } = board.events;
+  const event = message.event;
 
-	if (event.order <= log.getLastIndex()) {
-		return;
-	}
+  if (event.order <= log.getLastIndex()) {
+    return;
+  }
 
-	const eventUserId = parseFloat(event.body.eventId.split(':')[0]);
-	const currentUserId = +(new Storage().getUserId() || '0');
-	const isEventFromCurrentUser = eventUserId === currentUserId;
+  const eventUserId = parseFloat(event.body.eventId.split(":")[0]);
+  const currentUserId = Number(localStorage.getItem("userId") || "0");
+  const isEventFromCurrentUser = eventUserId === currentUserId;
 
-	if (isEventFromCurrentUser) {
-		return;
-	}
+  if (isEventFromCurrentUser) {
+    return;
+  }
 
-	log.insertEventsFromOtherConnections({
-		...event,
-		userId: message.userId,
-	});
-	const last = log.getLastConfirmed();
-	if (last) {
-		board.events.subject.publish(last);
-	}
+  log.insertEventsFromOtherConnections({
+    ...event,
+    userId: message.userId,
+  });
+  const last = log.getLastConfirmed();
+  if (last) {
+    board.events.subject.publish(last);
+  }
 }
--- src/Events/MessageRouter/handleModeMessage.ts
+++ src/Events/MessageRouter/handleModeMessage.ts
@@ -1,28 +1,46 @@
-import { Board } from 'Board';
-import { conf } from 'Settings';
-import { isTemplateView } from 'shared/lib/queryStringParser';
+import { Board } from "Board";
+import { conf } from "Settings";
 
-export type ViewMode = 'view' | 'edit' | 'loading';
+export type ViewMode = "view" | "edit" | "loading";
 
 export interface ModeMsg {
-	type: 'Mode';
-	boardId: string;
-	mode: ViewMode;
+  type: "Mode";
+  boardId: string;
+  mode: ViewMode;
 }
 
 export function handleModeMessage(message: ModeMsg, board: Board): void {
-	if (board.getInterfaceType() !== message.mode) {
-		board.setInterfaceType(message.mode);
-		if (isTemplateView()) {
-			return;
-		}
-		conf.notify({
-			header: conf.i18n.t('sharing.settingsChanged.heading'),
-			body:
-				message.mode === 'edit'
-					? conf.i18n.t('sharing.settingsChanged.bodyEdit')
-					: conf.i18n.t('sharing.settingsChanged.bodyView'),
-			duration: 5000,
-		});
-	}
+  if (board.getInterfaceType() !== message.mode) {
+    board.setInterfaceType(message.mode);
+    if (isTemplateView()) {
+      return;
+    }
+    conf.notify({
+      header: conf.i18n.t("sharing.settingsChanged.heading"),
+      body:
+        message.mode === "edit"
+          ? conf.i18n.t("sharing.settingsChanged.bodyEdit")
+          : conf.i18n.t("sharing.settingsChanged.bodyView"),
+      duration: 5000,
+    });
+  }
+}
+
+/** Default value is returned if did not find such param */
+function getBooleanParam(searchParam: string, defaultValue: boolean): boolean {
+  const param = new URLSearchParams(window.location.search).get(searchParam);
+  if (!param) {
+    return defaultValue;
+  }
+  return param === "true" ? true : false;
+}
+
+type PanelType = "titlePanel" | "userPanel";
+/** Tries to find panel in search param, returns default value if not found */
+export function shouldShow(panel: PanelType): boolean {
+  return getBooleanParam(panel, true);
+}
+
+export function isTemplateView(): boolean {
+  return getBooleanParam("isTemplateView", false);
 }
