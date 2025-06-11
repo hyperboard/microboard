c8853c642f90e4015c6af569fdf24a55af9defb5
Arsenii <rad.archeg@gmail.com>
2025-06-11 18:32:09 +0700

    fixed exports

--- src/Items/Shape/index.ts
+++ src/Items/Shape/index.ts
@@ -4,6 +4,6 @@ import { BPMN } from "./BPMN";
 export { Shape, Shapes } from "./Shape";
 export { ShapeCommand } from "./ShapeCommand";
 export type { ShapeOperation } from "./ShapeOperation";
-export type * from "./ShapeData";
+export * from "./ShapeData";
 export type ShapeType = keyof typeof BasicShapes | BPMN;
 export { DefaultShapeData } from "./ShapeData";
